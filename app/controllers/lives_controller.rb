ac89cbe8e5cd2c6f54831adc17448ae9847b76ef
sumino-hill <sirokichi.0823@gmail.com>
2021-06-21 14:23:40 +0900

    アイコン表示未完成

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -2,4 +2,10 @@ class LivesController < ApplicationController
   def index
     
   end
+
+  private
+
+  def live_params
+    params.require(:user).permit(:image,:nickname,:birthday,:image, :image_cache, :remove_image).merge(user_id: current_user.id, image: current_user.image)
+  end
 end
