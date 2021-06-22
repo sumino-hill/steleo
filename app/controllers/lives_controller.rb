2501103b124e541b7a12d96dbc2b92d651a13b6b
sumino-hill <sirokichi.0823@gmail.com>
2021-06-22 15:26:17 +0900

    carrierwaveの削除

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,11 +1,14 @@
 class LivesController < ApplicationController
   def index
-    
+  end
+
+  def new
+  end
+
+  def create
   end
 
   private
 
-  def live_params
-    params.require(:user).permit(:image,:nickname,:birthday,:image, :image_cache, :remove_image).merge(user_id: current_user.id, image: current_user.image)
-  end
+
 end
