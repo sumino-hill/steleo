fdaebd187405b0387cb0ba93a9167cc3a33de197
sumino-hill <sirokichi.0823@gmail.com>
2021-06-23 15:51:44 +0900

    投稿動画の保存

--- app/models/live.rb
+++ app/models/live.rb
@@ -3,4 +3,8 @@ class Live < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :atmosphere
+  with_options presence: true do
+    validates :image
+    validates :title
+  end
 end
